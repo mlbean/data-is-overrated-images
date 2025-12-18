bafce5979f958bad4e17e4576dbddfb185bdec67
bene <benediis@uio.no>
2025-12-18 01:27:33 +0100

    Fix dimensions in layers

--- model.py
+++ model.py
@@ -10,9 +10,9 @@ def encoder_block(loss_func, in_channels, out_channels, kernel_size, stride, pad
         loss_func(),
     )
 
-def decoder_block(loss_func, in_channels, out_channels, kernel_size, stride, padding):
+def decoder_block(loss_func, in_channels, out_channels, kernel_size, stride, padding, output_padding):
     return nn.Sequential(
-        nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, output_padding),
         nn.BatchNorm2d(out_channels),
         loss_func(),
     )
@@ -21,10 +21,9 @@ class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.encoder = nn.Sequential(
-            encoder_block(nn.LeakyReLU, 3, 128, 3, 2, 1),
-            encoder_block(nn.LeakyReLU, 128, 128, 3, 2, 1),
-            encoder_block(nn.Tanh, 128, 3, 3, 2, 1)
+        self.encoder = nn.Sequential(  # 1x28x28
+            encoder_block(nn.LeakyReLU, 1, 128, 3, 2, 1),  # 128x14x14
+            encoder_block(nn.Tanh, 128, 8, 3, 2, 1)  # 8x7x7
         )
         
         self.quantise = quantise
@@ -40,11 +39,10 @@ class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.decoder = nn.Sequential(
-            decoder_block(nn.LeakyReLU, 3, 128, 3, 2, 1),
-            decoder_block(nn.LeakyReLU, 128, 128, 3, 2, 1),
-            decoder_block(nn.LeakyReLU, 128, 128, 3, 2, 1),
-            nn.Conv2d(128, 3, 1, 1),
+        self.decoder = nn.Sequential(  # 8x7x7
+            decoder_block(nn.LeakyReLU, 8, 128, 3, 2, 1, 1),  #128x14x14
+            decoder_block(nn.LeakyReLU, 128, 128, 3, 2, 1, 1), #128x28x28
+            nn.Conv2d(128, 1, 3, 1, 1),  #128x28x28
             nn.Sigmoid()
         )
         
